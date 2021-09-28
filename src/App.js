fceb07bc106a86b437bae6b193e44a8344ca9b3c
Антон Діброва <anton.dibrova1113@gmail.com>
2021-09-28 16:41:16 +0300

    import Game and Info

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from "react";
+import Game from "./components/Game";
+import Info from "./components/Info";
 
 class App extends React.Component {
   render() {
     return (
       <div className = "main">
-
+        <Game />
+        <Info />
       </div>
     );
   }
