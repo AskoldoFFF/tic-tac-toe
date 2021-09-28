04eec07efd20def440848b10999465a8d598f12a
Антон Діброва <anton.dibrova1113@gmail.com>
2021-09-28 19:03:52 +0300

    added place for score

--- src/components/Info.js
+++ src/components/Info.js
@@ -3,8 +3,10 @@ import React from "react";
 class Info extends React.Component {
     render() {
         return (
-            <div>
-
+            <div className = "info-square">
+                <h1>Score</h1>
+                <p>Player1:</p>
+                <p>Player2:</p>
             </div>
         );
     }
