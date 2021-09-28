0dde9b66b3b63d836afddd122553cf3d133a782c
Антон Діброва <anton.dibrova1113@gmail.com>
2021-09-28 19:03:18 +0300

    added fields

--- src/components/Game.js
+++ src/components/Game.js
@@ -3,8 +3,16 @@ import React from "react";
 class Game extends React.Component {
     render() {
         return (
-            <div>
-
+            <div className = "game-square">
+                <div className = "game-field"></div>
+                <div className = "game-field"></div>
+                <div className = "game-field"></div>
+                <div className = "game-field"></div>
+                <div className = "game-field"></div>
+                <div className = "game-field"></div>
+                <div className = "game-field"></div>
+                <div className = "game-field"></div>
+                <div className = "game-field"></div>
             </div>
         );
     }
