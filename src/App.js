b87559bcdd3bc79449efb7a962a7ff06d5036fa8
Антон Діброва <anton.dibrova1113@gmail.com>
2021-09-29 20:13:00 +0300

    added fields for a score and windows to select name

--- src/App.js
+++ src/App.js
@@ -7,16 +7,34 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      
+      playerX: "PlayerX",
+      playerO: "PlayerO",
+      scorePlayerX: 0,
+      scorePlayerO: 0
     };
+  }
+
+  updateScorePlayerX = (scoreX) => {
+    this.setState({scorePlayerX: scoreX});
+  }
+
+  updateScorePlayerO = (scoreO) => {
+    this.setState({scorePlayerO: scoreO});
+  }
+
+  componentDidMount() {
+    this.setState({playerX: prompt("Введите имя игрока X:", "PlayerX")});
+    this.setState({playerO: prompt("Введите имя игрока O:", "PlayerO")});
   }
 
 
   render() {
     return (
       <div className = "main">
-        <Game />
-        <Info />
+        <Game playerX = {this.state.playerX} playerO = {this.state.playerO} 
+        updateScorePlayerX = {this.updateScorePlayerX} updateScorePlayerO = {this.updateScorePlayerO}/>
+        <Info playerX = {this.state.playerX} playerO = {this.state.playerO}
+         scorePlayerX = {this.state.scorePlayerX} scorePlayerO = {this.state.scorePlayerO} />
       </div>
     );
   }
