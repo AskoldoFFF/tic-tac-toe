ca3ff8a413e8bd40377f949e81780a0bde65888a
Антон Діброва <anton.dibrova1113@gmail.com>
2021-09-29 20:22:59 +0300

    fix null name

--- src/components/Info.js
+++ src/components/Info.js
@@ -5,8 +5,8 @@ class Info extends React.Component {
         return (
             <div className = "info-square">
                 <h1>Score</h1>
-                <p>{this.props.playerX}: {this.props.scorePlayerX}</p>
-                <p>{this.props.playerO}: {this.props.scorePlayerO}</p>
+                <p>{(this.props.playerX === null) ? "PlayerX" : this.props.playerX}: {this.props.scorePlayerX}</p>
+                <p>{(this.props.playerO === null) ? "PlayerO" : this.props.playerO}: {this.props.scorePlayerO}</p>
             </div>
         );
     }
